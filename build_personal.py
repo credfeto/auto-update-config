0bb5730ff1043a5b8248e14e121f6621f2561585
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-02-05 23:50:03 +0000

    Update build_personal.py

--- build_personal.py
+++ build_personal.py
@@ -16,7 +16,7 @@ def make_query(after_cursor=None):
     return """
 query {
   viewer {
-    repositories(first: 100, affiliations:[OWNER], after:AFTER) {
+    repositories(first: 100, affiliations:[OWNER], archived:false, after:AFTER) {
       pageInfo {
         hasNextPage
         endCursor
